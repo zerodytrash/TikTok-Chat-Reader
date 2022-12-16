661f7698c9a5d7fdb8c9e709e82642cc0c6168cf
AVS42071 <96913756+AVS42071@users.noreply.github.com>
2022-12-16 00:13:38 -0600

    Update demo_db_connection.js

--- public/demo_db_connection.js
+++ public/demo_db_connection.js
@@ -1,1 +1,11 @@
 var mysql = require('mysql');
+let dbconnection = mysql.createConnection({
+        host: 'dccia.ml',
+        user: 'ltqffwvi_tiktok',
+        password: 'YoMama11785!',
+        database: 'ltqffwvi_tiktok'
+    });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
