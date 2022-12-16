107c3273ca429c8df9e618ce3f5048ec0fe7a572
AVS42071 <96913756+AVS42071@users.noreply.github.com>
2022-12-16 00:32:23 -0600

    Delete demo_db_connection.js

--- public/demo_db_connection.js
+++
@@ -1,7 +0,0 @@
-var mysql = require('mysql');
-let dbconnection = mysql.createConnection({
-        host: 'dccia.ml',
-        user: 'ltqffwvi_tiktok',
-        password: 'YoMama11785!',
-        database: 'ltqffwvi_tiktok'
-    });
